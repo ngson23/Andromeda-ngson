637478c2256084f24d05320928b1ea11a01e04db
Son <sonnvph20319@fpt.edu.vn>
2022-11-28 13:32:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/andromeda/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.andromeda;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    SettingFragment settingsFragment = new SettingFragment();
+    ForumFragment forumFragment = new ForumFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+    DownloadFragment downloadFragment = new DownloadFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView  = findViewById(R.id.bottom_navigation);
+
+//      getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+
+
+
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,homeFragment).commit();
+                        return true;
+                    case R.id.forum:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,forumFragment).commit();
+                        return true;
+                    case R.id.download:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,downloadFragment).commit();
+                    case R.id.setting:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,settingsFragment).commit();
+                    case R.id.profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,profileFragment).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+    }
+}
